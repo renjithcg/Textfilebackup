0e1698748a26111a275f5961f2a750a4d588d919
renjith <renjithcg09@gmail.com>
2018-04-10 15:18:28 +0530

    After change

--- test.py
+++ test.py
@@ -1,27 +1,40 @@
 import os
 import sys
 import zipfile
+import os.path
 
 def Backup():
+    if os.path.exists("test.txt"):
+        os.remove("test.txt")
     try:
-        file = open("D:\\Renjith\\test.txt","w")
+        file = open("./test.txt","w")
         file.write("This is a sentence")
     except IOError:
         print("error")
     else:
         print("successfully Backup")
         file.close()
+    if os.path.exists("test.txt"):
+        print("File is Ready")
+    else:
+        print("Not Ready")
+    #a=os.path.join(os.getcwd(),"test" + "." + "txt")
+    #print(a)
+    #return (a)
 
 
 def zip():
     try:
-        jungle_zip = zipfile.ZipFile('D:\\Renjith\\test.txt' + '.zip', 'w')
-        jungle_zip.write('D:\\Renjith\\test.txt', compress_type=zipfile.ZIP_DEFLATED)
+        jungle_zip = zipfile.ZipFile('./test.txt' + '.zip', 'w')
+        jungle_zip.write('./test.txt', compress_type=zipfile.ZIP_DEFLATED)
     except IOError:
         print("not zipped")
     else:
         print("File zipped successfully")
         jungle_zip.close()
+        #b=os.path.join(os.getcwd(),"test" + "." + "zip")
+        #return (b)
+    #print(b)
 
 BACKUP_FILE=Backup()
 ZIP_FILE=zip()
